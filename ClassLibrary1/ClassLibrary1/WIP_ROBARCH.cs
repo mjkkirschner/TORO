14483a389955971e6fe3fdd7889d212d6d62258a
coten <nick.cote@autodesk.com>
2016-03-08 11:26:47 -0800

    Nick update to wiprobarch

--- ClassLibrary1/ClassLibrary1/WIP_ROBARCH.cs
+++ ClassLibrary1/ClassLibrary1/WIP_ROBARCH.cs
@@ -1030,7 +1030,8 @@ namespace Dynamo_TORO
 
             foreach (Plane p in planeList)
             {
-                double dot = p.XAxis.Dot(vec);
+                Vector projectedVector = Vector.ByCoordinates(p.XAxis.X, p.XAxis.Y, 0);
+                double dot = p.XAxis.Dot(projectedVector);
                 double angle = Math.Acos(dot) * (-1) * degree * (180 / Math.PI);
                 Plane newPl = (Plane)p.Rotate(p.Origin, p.Normal, angle);
                 newPlList.Add(newPl);
@@ -1053,7 +1054,8 @@ namespace Dynamo_TORO
 
             foreach (CoordinateSystem cs in csList)
             {
-                double dot = cs.XAxis.Dot(vec);
+                Vector projectedVector = Vector.ByCoordinates(cs.XAxis.X, cs.XAxis.Y, 0);
+                double dot = cs.XAxis.Dot(projectedVector);
                 double angle = Math.Acos(dot) * (-1) * degree * (180 / Math.PI);
                 CoordinateSystem newCS = cs.Rotate(cs.Origin, cs.ZAxis, angle);
                 newCSList.Add(newCS);
@@ -1080,7 +1082,8 @@ namespace Dynamo_TORO
                 foreach (Strut strut in node.Struts)
                 {
                     Plane p = strut.TransformedCutPlane;
-                    double dot = p.XAxis.Dot(vec);
+                    Vector projectedVector = Vector.ByCoordinates(p.XAxis.X, p.XAxis.Y, 0);
+                    double dot = p.XAxis.Dot(projectedVector);
                     double angle = Math.Acos(dot) * (-1) * degree * (180 / Math.PI);
 
                     // rebuild node
@@ -1177,7 +1180,7 @@ namespace Dynamo_TORO
         /// <param name="points">List of points</param>
         /// <returns></returns>
         [MultiReturn(new[] { "lineList", "planeList" })]
-        public static Dictionary<string, List<List<Object>>> getLinesAtPoint(List<Line> lines, List<Point> points)
+        public static Dictionary<string, List<List<Object>>> getLinesPlaneAtPoint(List<Line> lines, List<Point> points)
         {
             List<List<Object>> lineListList = new List<List<Object>>();
             List<List<Object>> planeListList = new List<List<Object>>();
@@ -1221,6 +1224,126 @@ namespace Dynamo_TORO
                 {"lineList", lineListList},
                 {"planeList", planeListList}
             };
+        }
+
+
+
+        /// <summary>
+        /// For each point get list of and reorient all lines and get list of planes.
+        /// </summary>
+        /// <param name="lines">List of lines</param>
+        /// <param name="points">List of points</param>
+        /// <returns></returns>
+        [MultiReturn(new[] { "lineList", "planeList" })]
+        public static Dictionary<string, List<List<Object>>> getLinesCoordSysAtPoint(List<Line> lines, List<Point> points)
+        {
+            List<List<Object>> lineListList = new List<List<Object>>();
+            List<List<Object>> CSListList = new List<List<Object>>();
+            List<Point> errPoints = new List<Point>();
+
+            foreach (Point p in points)
+            {
+                List<Object> lineList = new List<Object>();
+                List<Object> csList = new List<Object>();
+
+                List<bool> boolList = new List<bool>();
+
+                foreach (Line l in lines)
+                {
+                    bool intersection = p.DoesIntersect(l);
+                    if (intersection)
+                    {
+                        bool meets = l.StartPoint.IsAlmostEqualTo(p);
+                        if (meets)
+                        {
+                            lineList.Add(l);
+                            Plane pl = l.PlaneAtParameter(0);
+                            CoordinateSystem cs = CoordinateSystem.ByPlane(pl);
+                            csList.Add((Object)cs);
+                        }
+                        else
+                        {
+                            Line lRev = (Line)l.Reverse();
+                            lineList.Add((Object)lRev);
+                            Plane pl = lRev.PlaneAtParameter(0);
+                            CoordinateSystem cs = CoordinateSystem.ByPlane(pl);
+                            csList.Add((Object)cs);
+                        }
+                    }
+                }
+
+                lineListList.Add(lineList);
+                CSListList.Add(csList);
+            }
+
+            return new Dictionary<string, List<List<Object>>>
+            {
+                {"lineList", lineListList},
+                {"planeList", CSListList}
+            };
+        }
+
+
+
+        /// <summary>
+        /// Transform each sublist of planes to World XY plane using average ZAxis.
+        /// </summary>
+        /// <param name="planes">List of list of planes</param>
+        /// <param name="guide">Guide plane</param>
+        /// <returns></returns>
+        public static List<List<Plane>> transform_Plane(List<List<Plane>> planes, [DefaultArgumentAttribute("{Plane.ByOriginNormalXAxis(Point.ByCoordinates(0,0,0), Vector.ByCoordinates(0,0,1), Vector.ByCoordinates(1,0,0))}")] Plane guide)
+        {
+            List<List<Plane>> newPlanes = new List<List<Plane>>();
+
+            foreach (List<Plane> sub in planes)
+            {
+                Point origin = sub.Select(p => p.Origin).ToList()[0];
+                List<Vector> zAxis = sub.Select(p => p.Normal).ToList();
+                Vector zAxisAvg = Vector.ByCoordinates(zAxis.Average(v => v.X), zAxis.Average(v => v.Y), zAxis.Average(v => v.Z));
+                Plane subFrame = Plane.ByOriginNormal(origin, zAxisAvg);
+
+                CoordinateSystem subFrameTransform = subFrame.ContextCoordinateSystem;
+                CoordinateSystem guideFrame = guide.ContextCoordinateSystem;
+
+                List<Plane> newSub = new List<Plane>();
+                foreach (Plane plane in sub)
+                {
+                    newSub.Add((Plane) plane.Transform(subFrameTransform, guideFrame));
+                }
+                newPlanes.Add(newSub);
+            }
+            return newPlanes;
+        }
+
+
+
+        /// <summary>
+        /// Transform each sublist of coordinate systems to World XY coordinate systems using average ZAxis.
+        /// </summary>
+        /// <param name="coordSys">List of list of planes</param>
+        /// <param name="guide">Guide coordinate system</param>
+        /// <returns></returns>
+        public static List<List<CoordinateSystem>> transform_CoordSys(List<List<CoordinateSystem>> coordSys, [DefaultArgumentAttribute("{CoordinateSystem.ByPlane(Plane.ByOriginNormalXAxis(Point.ByCoordinates(0,0,0), Vector.ByCoordinates(0,0,1), Vector.ByCoordinates(1,0,0)))}")] CoordinateSystem guide)
+        {
+            List<List<CoordinateSystem>> newCoordSys = new List<List<CoordinateSystem>>();
+
+            foreach (List<CoordinateSystem> sub in coordSys)
+            {
+                Point origin = sub.Select(p => p.Origin).ToList()[0];
+                List<Vector> zAxis = sub.Select(p => p.ZAxis).ToList();
+                Vector zAxisAvg = Vector.ByCoordinates(zAxis.Average(v => v.X), zAxis.Average(v => v.Y), zAxis.Average(v => v.Z));
+                Plane subFrame = Plane.ByOriginNormal(origin, zAxisAvg);
+
+                CoordinateSystem subFrameTransform = subFrame.ContextCoordinateSystem;
+
+                List<CoordinateSystem> newSub = new List<CoordinateSystem>();
+                foreach (CoordinateSystem cs in sub)
+                {
+                    newSub.Add((CoordinateSystem) cs.Transform(subFrameTransform, guide));
+                }
+                newCoordSys.Add(newSub);
+            }
+            return newCoordSys;
         }
 
 
@@ -1462,7 +1585,7 @@ namespace Dynamo_TORO
                         "\n\t\t" + "ConfL\\Off;" +
                         "\n\t\t" + "SingArea\\Wrist;" +
                         "\n" +
-                        "\n\t\t" + "TPWrite(\"This is: " + filePrefix + "_" + paddedIndex + "\");" +
+                        "\n\t\t" + "TPWrite(\"This is " + filePrefix + "_" + paddedIndex + "\");" +
                         "\n\t\t" + "TPWrite(\"Check block and drill\");" +
                         "\n\t\t" + "MoveAbsJ j0, v100, z5, tool0;" +
                         "\n\t\t" + "MoveAbsJ j1, v100, z5, tool0;" + moveBuilder.ToString() +
@@ -1514,38 +1637,41 @@ namespace Dynamo_TORO
                 int index = 0;
                 var targBuilder = new StringBuilder();
                 var moveBuilder = new StringBuilder();
-                targBuilder.Append(string.Format("\n\tVAR jointtarget j0 := {0};", jtarget(-90, 0, 0, 90, 90, 0)));
-                targBuilder.Append(string.Format("\n\tVAR jointtarget j1 := {0};", jtarget(-41, 0, 0, 0, 90, 0)));
                 foreach (Strut strut in node.Struts)
                 {
                     // setup names
                     Plane hole = strut.TransformedCutPlane;
-                    string name = strut.ID;
 
                     // create targets
-                    targBuilder.Append(string.Format("\n\tVAR robtarget p{0}0 := {1};", index, rtarget((Plane)hole.Translate(hole.Normal, -120))));
-                    targBuilder.Append(string.Format("\n\tVAR robtarget p{0}1 := {1};", index, rtarget((Plane)hole.Translate(hole.Normal, -50))));
-                    targBuilder.Append(string.Format("\n\tVAR robtarget p{0}2 := {1};", index, rtarget((Plane)hole.Translate(hole.Normal, -10))));
+                    targBuilder.Append(string.Format("\n"));
+                    targBuilder.Append(string.Format("\n\t! {0};", strut.ID));
+                    targBuilder.Append(string.Format("\n\tVAR robtarget S{0}0 := {1};", index, rtarget((Plane)hole.Translate(hole.Normal, -120))));
+                    targBuilder.Append(string.Format("\n\tVAR robtarget S{0}1 := {1};", index, rtarget((Plane)hole.Translate(hole.Normal, -50))));
+                    targBuilder.Append(string.Format("\n\tVAR robtarget S{0}2 := {1};", index, rtarget((Plane)hole.Translate(hole.Normal, -10))));
 
                     // create movement instructions
                     moveBuilder.Append(string.Format("\n"));
-                    moveBuilder.Append(string.Format("\n\t\tTPWrite(\"Drilling hole for strut {0}! ({1}/{2})\");", name, index + 1, node.Struts.Count()));
-                    moveBuilder.Append(string.Format("\n\t\tMoveL p{0}0, {1}, {2}, drill\\WObj:=block;", index, "v200", "z30"));
-                    moveBuilder.Append(string.Format("\n\t\tMoveL p{0}1, {1}, {2}, drill\\WObj:=block;", index, "v100", "z5"));
-                    moveBuilder.Append(string.Format("\n\t\tMoveL p{0}2, {1}, {2}, drill\\WObj:=block;", index, "rate", "fine"));
-                    moveBuilder.Append(string.Format("\n\t\tMoveL p{0}1, {1}, {2}, drill\\WObj:=block;", index, "rate", "fine"));
-                    moveBuilder.Append(string.Format("\n\t\tMoveL p{0}0, {1}, {2}, drill\\WObj:=block;", index, "v100", "z5"));
-                    moveBuilder.Append(string.Format("\n\t\tMoveL RelTool(p{0}0, 0, 50, 0), {1}, {2}, drill\\WObj:=block;", index, "v200", "z30"));
+                    moveBuilder.Append(string.Format("\n\t\tTPWrite(\"Drilling... {0}, {1} of {2} ({3}%)\");", strut.ID, index + 1, node.Struts.Count(), Math.Round((double)(100 * (index + 1) / node.Struts.Count()))));
+                    moveBuilder.Append(string.Format("\n\t\tMoveL S{0}0, {1}, {2}, drill\\WObj:=block;", index, "v200", "z30"));
+                    moveBuilder.Append(string.Format("\n\t\tMoveL S{0}1, {1}, {2}, drill\\WObj:=block;", index, "v100", "z5"));
+                    moveBuilder.Append(string.Format("\n\t\tMoveL S{0}2, {1}, {2}, drill\\WObj:=block;", index, "rate", "fine"));
+                    moveBuilder.Append(string.Format("\n\t\tMoveL S{0}1, {1}, {2}, drill\\WObj:=block;", index, "rate", "fine"));
+                    moveBuilder.Append(string.Format("\n\t\tMoveL S{0}0, {1}, {2}, drill\\WObj:=block;", index, "v100", "z5"));
+                    moveBuilder.Append(string.Format("\n\t\tMoveL RelTool(S{0}0, 0, 50, 0), {1}, {2}, drill\\WObj:=block;", index, "v200", "z30"));
 
                     // create safe movement to next
                     if (index < node.Struts.Count() - 1)
                     {
-                        moveBuilder.Append(string.Format("\n\t\tMoveAbsJ CalcJointT(RelTool(p{0}0, 0, 50, 0), drill\\WObj:=block), {1}, {2}, drill\\WObj:=block;", index + 1, "v200", "z5"));
+                        moveBuilder.Append(string.Format("\n\t\tMoveAbsJ CalcJointT(RelTool(S{0}0, 0, 50, 0), drill\\WObj:=block), {1}, {2}, drill\\WObj:=block;", index + 1, "v200", "z5"));
                     }
 
                     // update index
                     index += 1;
                 }
+
+                targBuilder.Append(string.Format("\n"));
+                targBuilder.Append(string.Format("\n\tVAR jointtarget j0 := {0};", jtarget(-90, 0, 0, 90, 90, 0)));
+                targBuilder.Append(string.Format("\n\tVAR jointtarget j1 := {0};", jtarget(-41, 0, 0, 0, 90, 0)));
 
                 // create rapid
                 string r = "";
@@ -1554,22 +1680,27 @@ namespace Dynamo_TORO
                     r =
                         "MODULE MainModule" +
                         "\n" +
-                        "\n\t" + "! " + node.ID.ToString() +
-                        "\n" +
-                        "\n\t" + "! targets" + targBuilder.ToString() +
-                        "\n" +
-                        "\n\t" + "! drilling instructions" +
+
+                        "\n\t" + 
+                        "\n\t" + "! " + node.ID.ToString() + "\n" + targBuilder.ToString() +
+                        "\n\n\t" +
+
+                        "\n\t" + "! ROUTINE" +
                         "\n\t" + "PROC main()" +
                         "\n\t\t" + "ConfL\\Off;" +
                         "\n\t\t" + "SingArea\\Wrist;" +
                         "\n" +
-                        "\n\t\t" + "TPWrite(\"This is: " + node.ID.ToString() + "\");" +
-                        "\n\t\t" + "TPWrite(\"Check block and drill\");" +
-                        "\n\t\t" + "MoveAbsJ j0, v100, z5, tool0;" +
-                        "\n\t\t" + "MoveAbsJ j1, v100, z5, tool0;" + moveBuilder.ToString() +
-                        "\n\n\t\t" + "TPWrite(\"Resetting axes...\");" +
-                        "\n\t\t" + "MoveAbsJ j1, v100, z5, tool0;" +
-                        "\n\t\t" + "MoveAbsJ j0, v100, z5, tool0;" +
+                        "\n\t\t" + "TPWrite(\"This is " + node.ID.ToString() + "\");" +
+                        "\n" +
+                        "\n\t\t" + "TPWrite(\"Check block && drill!\");" +
+                        "\n\t\t" + "MoveAbsJ j0, v200, z5, tool0;" +
+                        "\n\t\t" + "MoveAbsJ j1, v200, z5, tool0;" + moveBuilder.ToString() +
+                        "\n" +
+                        "\n\t\t" + "TPWrite(\"Returning to start...\");" +
+                        "\n\t\t" + "MoveAbsJ j1, v200, z5, tool0;" +
+                        "\n\t\t" + "MoveAbsJ j0, v200, z5, tool0;" +
+                        "\n" +
+                        "\n\t\t" + "TPWrite(\"Node complete!\");" +
                         "\n" +
                         "\n\t\t" + "Stop;" +
                         "\n\t" + "ENDPROC" +
